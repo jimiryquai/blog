3c2d2ce4a5f2c781fd35857da19466d42a4f1d49
jimiryquai <james@jamesryan.dev>
2023-04-04 07:37:33 +0100

    added deb-build-test article

---
+++ src/components/PowerPlatformIcons.jsx
@@ -0,0 +1,135 @@
+export function PowerAppsIcon(props) {
+  return (
+    <svg width="96" height="96" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" {...props}>
+    <defs>
+        <filter id="filter0_f">
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="4" result="effect1_foregroundBlur" />
+        </filter>
+        <filter id="filter1_f">
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="0.38" result="effect1_foregroundBlur" />
+        </filter>
+        <linearGradient id="paint0_linear" x1="71" y1="96.0002" x2="15" y2="-40.9998"
+            gradientUnits="userSpaceOnUse">
+            <stop stopColor="#742774" />
+            <stop offset="1" stopColor="#BB35A4" />
+        </linearGradient>
+        <linearGradient id="paint1_linear" x1="32" y1="0.0002436" x2="78" y2="96.0002"
+            gradientUnits="userSpaceOnUse">
+            <stop stopColor="#E48CC7" />
+            <stop offset="1" stopColor="#D467B9" />
+        </linearGradient>
+        <linearGradient id="paint2_linear" x1="62" y1="29.0002" x2="76" y2="72"
+            gradientUnits="userSpaceOnUse">
+            <stop stopColor="#E9A1CB" />
+            <stop offset="1" stopColor="#E48CC7" />
+        </linearGradient>
+        <clipPath id="clip0">
+            <rect width="96" height="96" fill="white" />
+        </clipPath>
+        <clipPath id="clip1">
+            <rect width="96" height="96" fill="white" />
+        </clipPath>
+        <clipPath id="clip2">
+            <rect width="96" height="96" fill="white" transform="translate(0 0.00012207)" />
+        </clipPath>
+    </defs>
+    <g clip-path="url(#clip0)">
+        <g clip-path="url(#clip1)">
+            <g clip-path="url(#clip2)">
+                <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="-1" y="1"
+                    width="97" height="94">
+                    <path
+                        d="M1.45715 44.049C-0.522237 46.3111 -0.522238 49.6889 1.45715 51.951L36.9897 92.5596C38.5833 94.381 41.4167 94.381 43.0103 92.5596L46.8367 88.1866L51.0504 92.7833C52.6358 94.5128 55.3623 94.5128 56.9476 92.7833L94.2825 52.0543C96.3853 49.7604 96.3853 46.2396 94.2825 43.9457L56.9476 3.21667C55.3623 1.4872 52.6358 1.4872 51.0504 3.21667L46.8367 7.81341L43.0103 3.44035C41.4167 1.61904 38.5833 1.61904 36.9897 3.44035L1.45715 44.049Z"
+                        fill="white" />
+                </mask>
+                <g mask="url(#mask0)">
+                    <path d="M40 96L-2 48L40 0L82 48L40 96Z" fill="url(#paint0_linear)" />
+                    <g filter="url(#filter0_f)">
+                        <path
+                            d="M53.999 2L97.999 50L53.999 98L34.4767 76.7029C33.0748 75.1736 33.0748 72.8264 34.4767 71.2971L51.5214 52.7029C52.9232 51.1736 52.9232 48.8264 51.5214 47.2971L34.4767 28.7029C33.0748 27.1736 33.0748 24.8264 34.4767 23.2971L53.999 2Z"
+                            fill="black" fill-opacity="0.32" />
+                    </g>
+                    <g filter="url(#filter1_f)">
+                        <path
+                            d="M53.999 0.380127L97.999 48.3801L53.999 96.3801L34.4767 75.083C33.0748 73.5537 33.0748 71.2065 34.4767 69.6772L51.5214 51.083C52.9232 49.5537 52.9232 47.2065 51.5214 45.6772L34.4767 27.083C33.0748 25.5537 33.0748 23.2065 34.4767 21.6772L53.999 0.380127Z"
+                            fill="black" fill-opacity="0.24" />
+                    </g>
+                    <path
+                        d="M53.999 0L97.999 48L53.999 96L34.4767 74.7029C33.0748 73.1736 33.0748 70.8264 34.4767 69.2971L51.5214 50.7029C52.9232 49.1736 52.9232 46.8264 51.5214 45.2971L34.4767 26.7029C33.0748 25.1736 33.0748 22.8264 34.4767 21.2971L53.999 0Z"
+                        fill="url(#paint1_linear)" />
+                    <path
+                        d="M73.0514 27.2167C74.6367 25.4872 77.3633 25.4872 78.9486 27.2167L98 48L78.9486 68.7833C77.3633 70.5128 74.6367 70.5128 73.0514 68.7833L57.7165 52.0543C55.6137 49.7604 55.6137 46.2396 57.7165 43.9457L73.0514 27.2167Z"
+                        fill="url(#paint2_linear)" />
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
+  )
+}
+
+export function PowerAutomateIcon(props) {
+    return (
+     <svg width="96" height="96" viewBox="0 0 96 96" fill="none" xmlns="http://www.w3.org/2000/svg" ariaHidden="true" {...props}>
+    <defs>
+        <filter id="filter0_f">
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="0.4" result="effect1_foregroundBlur" />
+        </filter>
+        <filter id="filter1_f">
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="4" result="effect1_foregroundBlur" />
+        </filter>
+        <linearGradient id="paint0_linear" x1="43" y1="55" x2="29" y2="10"
+            gradientUnits="userSpaceOnUse">
+            <stop stopColor="#0D36A5" />
+            <stop offset="1" stopColor="#1152D4" />
+        </linearGradient>
+        <linearGradient id="paint1_linear" x1="46" y1="10" x2="46" y2="86"
+            gradientUnits="userSpaceOnUse">
+            <stop stopColor="#84CAFF" />
+            <stop offset="1" stopColor="#61B1FB" />
+        </linearGradient>
+        <linearGradient id="paint2_linear" x1="37.5" y1="10" x2="37.5" y2="86"
+            gradientUnits="userSpaceOnUse">
+            <stop stopColor="#3B90F5" />
+            <stop offset="1" stop-color="#2A78EE" />
+        </linearGradient>
+        <clipPath id="clip0">
+            <rect width="96" height="96" fill="white" />
+        </clipPath>
+        <clipPath id="clip1">
+            <rect width="96" height="96" fill="white" />
+        </clipPath>
+    </defs>
+    <g clipPath="url(#clip0)">
+        <g clipPath="url(#clip1)">
+            <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="-1" y="10" width="97"
+                height="76">
+                <path
+                    d="M61.2116 10C62.3496 10 63.4337 10.4847 64.1925 11.3328L94.6136 45.3328C95.9723 46.8514 95.9723 49.1486 94.6136 50.6672L64.1925 84.6672C63.4337 85.5153 62.3496 86 61.2116 86H3.94634C0.488777 86 -1.34012 81.9095 0.965366 79.3328L29 48L0.965366 16.6672C-1.34012 14.0905 0.488777 10 3.94634 10H61.2116Z"
+                    fill="white" />
+            </mask>
+            <g mask="url(#mask0)">
+                <path d="M63 10L29 48L-5 10H63Z" fill="url(#paint0_linear)" />
+                <g filter="url(#filter0_f)">
+                    <path d="M63 10.4L-5 86.4H63L97 48.4L63 10.4Z" fill="black" fillOpacity="0.24" />
+                </g>
+                <g filter="url(#filter1_f)">
+                    <path d="M63 12L-5 88H63L97 50L63 12Z" fill="black" fillOpacity="0.32" />
+                </g>
+                <path d="M-5 86L63 10L97 48L63 86H-5Z" fill="url(#paint1_linear)" />
+                <path d="M-5 86L63 10L80 29L29 86H-5Z" fill="url(#paint2_linear)" />
+            </g>
+        </g>
+    </g>
+</svg>
+    )
+
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx'
 
-import { Button } from '@/components/Button'
+// import { Button } from '@/components/Button'
 import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
 import {
@@ -12,10 +12,10 @@ import {
   LinkedInIcon,
   TwitterIcon,
 } from '@/components/SocialIcons'
-import logoAirbnb from '@/images/logos/airbnb.svg'
-import logoFacebook from '@/images/logos/facebook.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
-import logoStarbucks from '@/images/logos/starbucks.svg'
+// import logoAirbnb from '@/images/logos/airbnb.svg'
+// import logoFacebook from '@/images/logos/facebook.svg'
+// import logoPlanetaria from '@/images/logos/planetaria.svg'
+// import logoStarbucks from '@/images/logos/starbucks.svg'
 import image1 from '@/images/photos/image-1.jpg'
 import image2 from '@/images/photos/image-2.jpg'
 import image3 from '@/images/photos/image-3.jpg'
@@ -24,6 +24,7 @@ import image5 from '@/images/photos/image-5.jpg'
 import { formatDate } from '@/lib/formatDate'
 import { generateRssFeed } from '@/lib/generateRssFeed'
 import { getAllArticles } from '@/lib/getAllArticles'
+import { PowerAppsIcon,PowerAutomateIcon } from '@/components/PowerPlatformIcons'
 
 function MailIcon(props) {
   return (
@@ -84,6 +85,8 @@ function ArrowDownIcon(props) {
   )
 }
 
+
+
 function Article({ article }) {
   return (
     <Card as="article">
@@ -107,144 +110,178 @@ function SocialLink({ icon: Icon, ...props }) {
   )
 }
 
-function Newsletter() {
-  return (
-    <form
-      action="/thank-you"
-      className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
-    >
-      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-        <MailIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Stay up to date</span>
-      </h2>
-      <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-        Get notified when I publish something new, and unsubscribe at any time.
-      </p>
-      <div className="mt-6 flex">
-        <input
-          type="email"
-          placeholder="Email address"
-          aria-label="Email address"
-          required
-          className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
-        />
-        <Button type="submit" className="ml-4 flex-none">
-          Join
-        </Button>
-      </div>
-    </form>
-  )
-}
+// function Newsletter() {
+//   return (
+//     <form
+//       action="/thank-you"
+//       className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
+//     >
+//       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+//         <MailIcon className="h-6 w-6 flex-none" />
+//         <span className="ml-3">Stay up to date</span>
+//       </h2>
+//       <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+//         Get notified when I publish something new, and unsubscribe at any time.
+//       </p>
+//       <div className="mt-6 flex">
+//         <input
+//           type="email"
+//           placeholder="Email address"
+//           aria-label="Email address"
+//           required
+//           className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
+//         />
+//         <Button type="submit" className="ml-4 flex-none">
+//           Join
+//         </Button>
+//       </div>
+//     </form>
+//   )
+// }
 
-function Resume() {
-  let resume = [
-    {
-      company: 'Planetaria',
-      title: 'CEO',
-      logo: logoPlanetaria,
-      start: '2019',
-      end: {
-        label: 'Present',
-        dateTime: new Date().getFullYear(),
-      },
-    },
-    {
-      company: 'Airbnb',
-      title: 'Product Designer',
-      logo: logoAirbnb,
-      start: '2014',
-      end: '2019',
-    },
-    {
-      company: 'Facebook',
-      title: 'iOS Software Engineer',
-      logo: logoFacebook,
-      start: '2011',
-      end: '2014',
-    },
-    {
-      company: 'Starbucks',
-      title: 'Shift Supervisor',
-      logo: logoStarbucks,
-      start: '2008',
-      end: '2011',
-    },
-  ]
+// function Resume() {
+//   let resume = [
+//     {
+//       company: 'Planetaria',
+//       title: 'CEO',
+//       logo: logoPlanetaria,
+//       start: '2019',
+//       end: {
+//         label: 'Present',
+//         dateTime: new Date().getFullYear(),
+//       },
+//     },
+//     {
+//       company: 'Airbnb',
+//       title: 'Product Designer',
+//       logo: logoAirbnb,
+//       start: '2014',
+//       end: '2019',
+//     },
+//     {
+//       company: 'Facebook',
+//       title: 'iOS Software Engineer',
+//       logo: logoFacebook,
+//       start: '2011',
+//       end: '2014',
+//     },
+//     {
+//       company: 'Starbucks',
+//       title: 'Shift Supervisor',
+//       logo: logoStarbucks,
+//       start: '2008',
+//       end: '2011',
+//     },
+//   ]
 
-  return (
-    <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
-      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-        <BriefcaseIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Work</span>
-      </h2>
-      <ol className="mt-6 space-y-4">
-        {resume.map((role, roleIndex) => (
-          <li key={roleIndex} className="flex gap-4">
-            <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-              <Image src={role.logo} alt="" className="h-7 w-7" unoptimized />
-            </div>
-            <dl className="flex flex-auto flex-wrap gap-x-2">
-              <dt className="sr-only">Company</dt>
-              <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
-                {role.company}
-              </dd>
-              <dt className="sr-only">Role</dt>
-              <dd className="text-xs text-zinc-500 dark:text-zinc-400">
-                {role.title}
-              </dd>
-              <dt className="sr-only">Date</dt>
-              <dd
-                className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
-                aria-label={`${role.start.label ?? role.start} until ${
-                  role.end.label ?? role.end
-                }`}
-              >
-                <time dateTime={role.start.dateTime ?? role.start}>
-                  {role.start.label ?? role.start}
-                </time>{' '}
-                <span aria-hidden="true">—</span>{' '}
-                <time dateTime={role.end.dateTime ?? role.end}>
-                  {role.end.label ?? role.end}
-                </time>
-              </dd>
-            </dl>
-          </li>
-        ))}
-      </ol>
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
-        Download CV
-        <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
-      </Button>
-    </div>
-  )
-}
+//   return (
+//     <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+//       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+//         <BriefcaseIcon className="h-6 w-6 flex-none" />
+//         <span className="ml-3">Work</span>
+//       </h2>
+//       <ol className="mt-6 space-y-4">
+//         {resume.map((role, roleIndex) => (
+//           <li key={roleIndex} className="flex gap-4">
+//             <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+//               <Image src={role.logo} alt="" className="h-7 w-7" unoptimized />
+//             </div>
+//             <dl className="flex flex-auto flex-wrap gap-x-2">
+//               <dt className="sr-only">Company</dt>
+//               <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
+//                 {role.company}
+//               </dd>
+//               <dt className="sr-only">Role</dt>
+//               <dd className="text-xs text-zinc-500 dark:text-zinc-400">
+//                 {role.title}
+//               </dd>
+//               <dt className="sr-only">Date</dt>
+//               <dd
+//                 className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
+//                 aria-label={`${role.start.label ?? role.start} until ${
+//                   role.end.label ?? role.end
+//                 }`}
+//               >
+//                 <time dateTime={role.start.dateTime ?? role.start}>
+//                   {role.start.label ?? role.start}
+//                 </time>{' '}
+//                 <span aria-hidden="true">—</span>{' '}
+//                 <time dateTime={role.end.dateTime ?? role.end}>
+//                   {role.end.label ?? role.end}
+//                 </time>
+//               </dd>
+//             </dl>
+//           </li>
+//         ))}
+//       </ol>
+//       <Button href="#" variant="secondary" className="group mt-6 w-full">
+//         Download CV
+//         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
+//       </Button>
+//     </div>
+//   )
+// }
 
-function Photos() {
-  let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
+// function Photos() {
+//   let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
 
-  return (
-    <div className="mt-16 sm:mt-20">
-      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
-          <div
-            key={image.src}
-            className={clsx(
-              'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
-              rotations[imageIndex % rotations.length]
-            )}
-          >
-            <Image
-              src={image}
-              alt=""
-              sizes="(min-width: 640px) 18rem, 11rem"
-              className="absolute inset-0 h-full w-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className="mt-16 sm:mt-20">
+//       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+//         {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
+//           <div
+//             key={image.src}
+//             className={clsx(
+//               'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
+//               rotations[imageIndex % rotations.length]
+//             )}
+//           >
+//             <Image
+//               src={image}
+//               alt=""
+//               sizes="(min-width: 640px) 18rem, 11rem"
+//               className="absolute inset-0 h-full w-full object-cover"
+//             />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   )
+// }
+
+// function LogoCloud() {
+//   return (
+//     <div className="mt-16 sm:mt-20">
+//       <div className="mx-auto max-w-7xl px-6 lg:px-8">
+//         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+//           <PowerAppsIcon />
+//           <PowerAutomateIcon />
+//           <img
+//             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+//             src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+//             alt="Tuple"
+//             width={158}
+//             height={48}
+//           />
+//           <img
+//             className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+//             src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+//             alt="SavvyCal"
+//             width={158}
+//             height={48}
+//           />
+//           <img
+//             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+//             src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+//             alt="Statamic"
+//             width={158}
+//             height={48}
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
 export default function Home({ articles }) {
   return (
@@ -290,7 +327,8 @@ export default function Home({ articles }) {
           </div>
         </div>
       </Container>
-      <Photos />
+      {/* <Photos /> */}
+      {/* <LogoCloud /> */}
       <Container className="mt-24 md:mt-28">
         {/* <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2"> */}
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none">
