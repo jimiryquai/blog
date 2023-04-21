a8e8673e24b519dc57de592b28eed8293ca5166b
jimiryquai <james@jamesryan.dev>
2023-04-21 13:02:13 +0100

    Updated index.js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -44,7 +44,7 @@ export default function Home({ articles }) {
         </title>
         <meta
           name="description"
-          content="I’m James, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
+          content="I’m James, a Power Platform Consultant based in the UK."
         />
       </Head>
       <Container className="mt-9">
