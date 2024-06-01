7547110a38d5f7933b00ba4b752dc763556ae039
jimiryquai <james@jamesryan.dev>
2024-06-01 10:43:08 +0100

    added post component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
+import { th } from 'date-fns/locale';
+
 const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+      aspectRatio: {
+        thumbnail: '1.5',
+      },
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
