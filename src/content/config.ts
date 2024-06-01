3ea29e9a7c7e9275816dec36e589ebe904d0bf60
jimiryquai <james@jamesryan.dev>
2024-06-01 09:29:02 +0100

    added content and schema

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import {defineCollection, z} from "astro:content";
+import {format} from "date-fns";
+
+const postsCollection = defineCollection({
+  schema: z.object({
+    author: z.string(),
+    categories: z.array(z.string()),
+    date: z.string().transform(str => format(new Date(str), "MMMM dd, yyyy")),
+    featured: z.boolean().default(false),
+    image: z.string(),
+    title: z.string(),
+  })
+});
+
+export const collections = {
+    posts: postsCollection
+};
